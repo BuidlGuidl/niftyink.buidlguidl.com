d6c92714febb6301310d748decb60c068db232fb
Shiv Bhonde <shivbhonde04@gmail.com>
2025-01-06 16:59:10 +0530

    update ponder url

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -19,7 +19,7 @@ import "~~/styles/globals.css";
 const shareTechMono = Share_Tech_Mono({ subsets: ["latin"], weight: "400" });
 
 const urqlClient = new Client({
-  url: "http://localhost:42069",
+  url: "https://bg-ponder-indexer-production.up.railway.app/",
   exchanges: [cacheExchange, fetchExchange],
 });
 
